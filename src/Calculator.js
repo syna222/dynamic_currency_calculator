4b4d6dfbf2c4a0a02a73d4a23cd8b5fbc0f4c3e3
Viola Kaemmer <v.kaemmer@gmx.de>
2022-12-25 23:16:08 +0100

    added problem-folder, added components Calculator and CurrencyInput.

---
+++ src/Calculator.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react';
+import CurrencyInput from './CurrencyInput';
+
+export default function Calculator(){
+
+    const [value, setValue ] = useState("");
+    const [currency, setCurrency] = useState("");   //declares where current input is coming from (which currency input)
+    const [exchangeRates, setExchangeRates] = useState([]);     //better use useRef-Hook?
+
+    function fetchRates(){
+        const arrOfObjs = [{from: "EUR", to: "USD"}, {from: "EUR", to: "GBP"}, {from: "USD", to: "EUR"}, {from: "USD", to: "GBP"}, {from: "GBP", to: "EUR"}, {from: "GBP", to: "USD"}];
+        for(let i in arrOfObjs){
+            let from = arrOfObjs[i].from;
+            let to = arrOfObjs[i].to;
+            let fromTo = from + to;       //key for object to append to exchangeRates
+            let URL = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
+            //console.log(fromTo);
+            //console.log(URL);
+            //fetch stuff:
+            fetch(URL)
+            .then(response => response.json())   //turn response text to json and then to js object
+            .then(json => {let arraySaved = exchangeRates; arraySaved.push({[fromTo] : json.result}); setExchangeRates(arraySaved)})   //save json.result (this is the current exchange rate) //fromTo needs to be in [] in order to set key of object with variable!
+            .catch(error => console.log(error));
+        }
+    }
+
+    useEffect(() => {
+        fetchRates();
+        console.log("these are the current exchange rates: ", exchangeRates);
+    }, []);
+
+
+
+    function convert(value, convertTo){
+        const key = currency + convertTo;       //e.g. "EURUSD"
+        //check if key is valid currency-expression:
+        const validCurrencies = ["EURUSD", "EURGBP", "USDEUR", "USDGBP", "GBPEUR", "GBPUSD"];
+        if(validCurrencies.includes(key)){
+            //console.log("test, der key in convert() ist: ", key);
+            //console.log("test, die exchangeRates sind: ", exchangeRates);
+            const rate = exchangeRates.find(obj => obj[key])[key];     //returns value in exchangeRates in object like in {EURUSD: } 
+            return value * rate;
+        } else{
+            return "";
+        }
+    }
+
+    //test:
+    //console.log("state-var value: ", value);
+    //console.log("state-var currency: ", currency);
+
+    const euro = currency !== "EUR" ? convert(value, "EUR") : value;
+    //console.log("euro:", euro);
+    const usd = currency !== "USD" ? convert(value, "USD") : value;
+    //console.log("usd: ", usd);
+    const gbp = currency !== "GBP" ? convert(value, "GBP") : value;
+    //console.log("gbp: ", gbp);
+
+    return(
+        <>
+            <h1>Show value: </h1>
+            <CurrencyInput ownCurr="EUR" myVal={euro} setVal={setValue} setCurr={setCurrency}/>
+            <br/>
+            <CurrencyInput ownCurr="USD" myVal={usd} setVal={setValue} setCurr={setCurrency}/>
+            <br/>
+            <CurrencyInput ownCurr="GBP" myVal={gbp} setVal={setValue} setCurr={setCurrency}/>
+        </>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/CurrencyInput.js
@@ -0,0 +1,28 @@
+
+export default function CurrencyInput({ownCurr, myVal, setVal, setCurr}){
+
+
+    function handleChange(e){       //e = event (here: onChange, target here: input)
+        setVal(e.target.value);     //set parent's state-var "value" to value in this input field
+        setCurr(ownCurr);           //set parent's state-var "currency" to currency of this input field
+    }
+    //test:
+    //console.log("coming from input with currency: ", ownCurr, " myVal is: ", myVal);
+
+    return (
+        <div>
+            <label>Enter value in {ownCurr}: </label>
+            <input type="text" value={myVal} onChange={handleChange}/>       
+        </div>);
+}
+
+
+
+
+
+
+
+
+
+
+
